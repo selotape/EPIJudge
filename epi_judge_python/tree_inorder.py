818a9ad634bd35d18bec5cc128254ca1574602f1
selotape <RonVisbord@gmail.com>
2018-07-15 18:11:46 +0300

    implement inorder traversal with O(1) space complexity

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -2,8 +2,24 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree):
-    # TODO - you fill in here.
-    return []
+    return list(inorder_O1_space(tree))
+
+
+def inorder_O1_space(t):
+    prev = None
+    while t:
+        if prev is t.parent:
+            if t.left:
+                _next = t.left
+            else:
+                yield t.data
+                _next = t.right or t.parent
+        elif prev is t.left:
+            yield t.data
+            _next = t.right or t.parent
+        else:  # prev is t.right
+            _next = t.parent
+        prev, t = t, _next
 
 
 if __name__ == '__main__':

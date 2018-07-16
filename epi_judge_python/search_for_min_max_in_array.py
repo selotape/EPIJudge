0a928f5feab1f388f943534db4bce057ef492560
selotape <RonVisbord@gmail.com>
2018-07-16 16:24:31 +0300

    search min and max simultaneously

--- epi_judge_python/search_for_min_max_in_array.py
+++ epi_judge_python/search_for_min_max_in_array.py
@@ -7,8 +7,37 @@ MinMax = collections.namedtuple('MinMax', ('smallest', 'largest'))
 
 
 def find_min_max(A):
-    # TODO - you fill in here.
-    return MinMax(0, 0)
+    if not A:
+        return MinMax(None, None)
+    elif len(A) == 1:
+        return MinMax(A[0], A[0])
+    else:
+        return MinMax(*_find_min_max(A))
+
+
+def _find_min_max(A):
+    min_c, max_c = float('inf'), float('-inf')
+
+    for i in range(len(A)//2):
+        min_c, max_c = _update_min_max(min_c, max_c, A[i*2], A[i*2+1])
+
+    if len(A) % 2:
+        min_c, max_c = _consider_last(min_c, max_c, A[-1])
+    return min_c, max_c
+
+
+def _update_min_max(min_c, max_c, l, r):
+    smaller, larger = (l, r) if l < r else (r, l)
+    return min(min_c, smaller), max(max_c, larger)
+
+
+def _consider_last(min_c, max_c, last):
+    if last is not None:
+        if last < min_c:
+            min_c = last
+        elif last > max_c:
+            max_c = last
+    return min_c, max_c
 
 
 def res_printer(prop, value):

3c9c5bcebbbcca71760515783a561713c5834d66
selotape <RonVisbord@gmail.com>
2018-07-16 10:51:53 +0300

    sort an increasing-decreasing array

--- epi_judge_python/sort_increasing_decreasing_array.py
+++ epi_judge_python/sort_increasing_decreasing_array.py
@@ -1,9 +1,30 @@
+from sorted_arrays_merge import merge_sorted_arrays
 from test_framework import generic_test
+
+_INCREASING = 1
+_DECREASING = -1
 
 
 def sort_k_increasing_decreasing_array(A):
-    # TODO - you fill in here.
-    return []
+    sorted_arrays, next_sorted_arr = [], []
+    direction = _INCREASING
+    prev = float('-inf')
+    for item in A:
+        if continues_direction(item, prev, direction):
+            next_sorted_arr.append(item)
+        else:
+            sorted_arrays.append(next_sorted_arr)
+            next_sorted_arr = [item]
+            direction *= 1
+        prev = item
+
+    sorted_arrays.append(next_sorted_arr)
+
+    return merge_sorted_arrays(sorted_arrays)
+
+
+def continues_direction(item, prev, direction):
+    return item >= prev if direction == _INCREASING else item <= prev
 
 
 if __name__ == '__main__':

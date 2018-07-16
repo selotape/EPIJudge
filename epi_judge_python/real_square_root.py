d599b829b477f3c90d2b9ed3a267ee4f1a8929d2
selotape <RonVisbord@gmail.com>
2018-07-16 14:27:44 +0300

    calculate square root with binary search

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,9 +1,24 @@
+from math import isclose
+
 from test_framework import generic_test
 
 
 def square_root(x):
-    # TODO - you fill in here.
-    return 0.0
+    if x in (0.0, 1.0):
+        return x
+    _from = 0.0
+    _to = x if x >= 1.0 else 1.0
+    mid = (_from + _to) / 2
+    mid_squared = mid ** 2
+    while not isclose(mid_squared, x, abs_tol=0.0000000000000000001):
+        if mid_squared > x:
+            _to = mid
+        else:
+            _from = mid
+        mid = (_from + _to) / 2
+        mid_squared = mid ** 2
+
+    return mid
 
 
 if __name__ == '__main__':

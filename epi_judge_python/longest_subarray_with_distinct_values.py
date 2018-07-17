ec60149d0ce3885b07cd5ec4d105d119e7858a87
selotape <RonVisbord@gmail.com>
2018-07-17 13:59:58 +0300

    longest subarray with distinct values

--- epi_judge_python/longest_subarray_with_distinct_values.py
+++ epi_judge_python/longest_subarray_with_distinct_values.py
@@ -2,8 +2,26 @@ from test_framework import generic_test
 
 
 def longest_subarray_with_distinct_entries(A):
-    # TODO - you fill in here.
-    return 0
+    last_seen = {}
+    best = 0
+    subarray_size = 0
+
+    for i, item in enumerate(A):
+        if _item_in_current_subarray(i, subarray_size, last_seen, item):
+            subarray_size = i - last_seen[item]
+        else:
+            subarray_size += 1
+        best = max(best, subarray_size)
+        last_seen[item] = i
+
+    return best
+
+
+def _item_in_current_subarray(i, subarray_size, last_seen, item):
+    if item not in last_seen:
+        return False
+    subarray_start = i - subarray_size
+    return subarray_start <= last_seen[item]
 
 
 if __name__ == '__main__':

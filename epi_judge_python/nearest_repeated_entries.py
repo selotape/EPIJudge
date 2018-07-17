0511d47066b124da774cc2a64fec3defa246200f
selotape <RonVisbord@gmail.com>
2018-07-17 10:50:08 +0300

    find nearest repetition in paragraph

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,9 +1,17 @@
+from collections import defaultdict
+
 from test_framework import generic_test
 
 
 def find_nearest_repetition(paragraph):
-    # TODO - you fill in here.
-    return 0
+    last_seen = defaultdict(lambda: float('-inf'))
+    closest_dist = float('inf')
+    for i, word in enumerate(paragraph):
+        last_seen_dist = i - last_seen[word]
+        if last_seen_dist < closest_dist:
+            closest_dist = last_seen_dist
+        last_seen[word] = i
+    return closest_dist if closest_dist != float('inf') else -1
 
 
 if __name__ == '__main__':

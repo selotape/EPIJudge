52d3cc1a890e50b80b66f34861feef39db35257a
selotape <RonVisbord@gmail.com>
2018-07-18 18:27:28 +0300

    two dynamic programming tasks

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -1,9 +1,23 @@
+from functools import lru_cache
+
 from test_framework import generic_test
 
 
 def num_combinations_for_final_score(final_score, individual_play_scores):
-    # TODO - you fill in here.
-    return 0
+    @lru_cache(maxsize=None)
+    def _combs(final_score, plays):
+        if final_score < 0:
+            return 0
+        if final_score == 0:
+            return 1
+        if not plays:
+            return 0
+        else:
+            first_play = plays[0]
+            possible_scores = range(0, final_score+1, first_play)
+            return sum(_combs(final_score - score, plays[1:]) for score in possible_scores)
+
+    return _combs(final_score, tuple(individual_play_scores))
 
 
 if __name__ == '__main__':
--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def number_of_ways(n, m):
-    # TODO - you fill in here.
-    return 0
+    cells = [[0 for _ in range(m)] for _ in range(n)]
+    for i in range(m):
+        cells[n-1][i] = 1
+    for i in range(n):
+        cells[i][m-1] = 1
+
+    for i in reversed(range(0, n - 1)):
+        for j in reversed(range(0, m - 1)):
+            cells[i][j] = cells[i + 1][j] + cells[i][j + 1]
+
+    return cells[0][0]
 
 
 if __name__ == '__main__':

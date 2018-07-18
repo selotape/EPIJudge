d6b70cb225d4d2e37092ad34669f46f19d007a23
selotape <RonVisbord@gmail.com>
2018-07-18 14:32:25 +0300

    enumerate palindromic decompositions

--- epi_judge_python/enumerate_palindromic_decompositions.py
+++ epi_judge_python/enumerate_palindromic_decompositions.py
@@ -2,8 +2,29 @@ from test_framework import generic_test
 
 
 def palindrome_decompositions(input):
-    # TODO - you fill in here.
-    return []
+    results = []
+
+    def decompose(offset, partial_decomposition):
+        if offset == len(input):
+            results.append(list(partial_decomposition))
+            return
+
+        for new_offset in range(offset + 1, len(input) + 1):
+            if _is_palindrome(input, offset, new_offset):
+                partial_decomposition.append(input[offset:new_offset])
+                decompose(new_offset, partial_decomposition)
+                partial_decomposition.pop()
+
+    decompose(0, [])
+    return results
+
+
+def _is_palindrome(input, _from, _to):
+    while _from < _to:
+        if input[_from] != input[_to - 1]:
+            return False
+        _from, _to = _from + 1, _to - 1
+    return True
 
 
 def comp(a, b):

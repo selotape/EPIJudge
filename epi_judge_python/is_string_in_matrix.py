c770d2e368193b5c1d86262d5bf5c2103183f7ad
selotape <RonVisbord@gmail.com>
2018-07-19 09:16:27 +0300

    does pattern exist in grid

--- epi_judge_python/is_string_in_matrix.py
+++ epi_judge_python/is_string_in_matrix.py
@@ -1,9 +1,27 @@
+import functools
+
 from test_framework import generic_test
 
 
 def is_pattern_contained_in_grid(grid, S):
-    # TODO - you fill in here.
-    return True
+    n = len(grid)
+    m = len(grid[0])
+
+    def adjacencies(i, j):
+        for a_i, a_j in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
+            if (a_i, a_j) != (i, j) and 0 <= a_i < n and 0 <= a_j < m:
+                yield (a_i, a_j)
+
+    @functools.lru_cache(maxsize=None)
+    def can_pattern_start_from(pat, i, j):
+        if grid[i][j] != pat[0]:
+            return False
+        if len(pat) == 1:
+            return True
+
+        return any(can_pattern_start_from(pat[1:], a_i, a_j) for a_i, a_j in adjacencies(i, j))
+
+    return any(can_pattern_start_from(tuple(S), i, j) for i in range(n) for j in range(m))
 
 
 if __name__ == '__main__':

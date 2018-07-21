a71ef5b184b1bc55f2950c1a96538a62344a7ba7
selotape <RonVisbord@gmail.com>
2018-07-21 15:10:16 +0300

    reverse string

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -1,4 +1,5 @@
 import functools
+from itertools import chain
 
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
@@ -6,8 +7,20 @@ from test_framework.test_utils import enable_executor_hook
 
 # Assume s is a string encoded as bytearray.
 def reverse_words(s):
-    # TODO - you fill in here.
-    return
+    s.reverse()
+
+    word_start = 0
+    for cur_char, c in enumerate(chain(s, [32])):
+        if c == 32:
+            reverse_word(s, word_start, cur_char - 1)
+            word_start = cur_char + 1
+
+
+def reverse_word(s, _from, _to):
+    for _ in range((_to - _from + 1) // 2):
+        s[_from], s[_to] = s[_to], s[_from]
+        _from += 1
+        _to -= 1
 
 
 @enable_executor_hook

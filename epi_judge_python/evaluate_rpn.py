27eb0fa96f2c1151f75cafe63658b52c59360feb
selotape <RonVisbord@gmail.com>
2018-07-22 10:09:47 +0300

    evaluate reverse-polish-notation

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,32 @@
+import operator
+from collections import deque
+from typing import Sequence
+
 from test_framework import generic_test
 
 
 def evaluate(expression):
-    # TODO - you fill in here.
-    return 0
+    segments = expression.split(',')
+    return _evaluate(segments)
+
+
+_ops = {
+    '*': operator.mul,
+    '/': operator.floordiv,
+    '+': operator.add,
+    '-': operator.sub,
+}
+
+def _evaluate(segments: Sequence[str]):
+    q = deque()
+    for w in segments:
+        if w in _ops:
+            r = q.pop()
+            l = q.pop()
+            q.append(_ops[w](l, r))
+        else:
+            q.append(int(w))
+    return q.pop()
 
 
 if __name__ == '__main__':

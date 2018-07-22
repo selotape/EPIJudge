64501f174526934c35ca2d334eb86ec1903dad0c
selotape <RonVisbord@gmail.com>
2018-07-22 13:20:58 +0300

    circular Q

--- epi_judge_python/circular_queue.py
+++ epi_judge_python/circular_queue.py
@@ -4,20 +4,43 @@ from test_framework.test_failure import TestFailure
 
 class Queue:
     def __init__(self, capacity):
-        # TODO - you fill in here.
-        return
+        self._data = []
+        self._head, self._tail, self._capacity = 0, 0, capacity
+        self._resize(capacity)
+
+    def _resize(self, new_capacity):
+        items = list(self)
+        self._capacity = new_capacity
+        self._data = [None] * new_capacity
+        self._head, self._tail = 0, 0
+        for item in items:
+            self._enqueue(item)
 
     def enqueue(self, x):
-        # TODO - you fill in here.
-        return
+        if self.size() == self._capacity:
+            self._resize(self._capacity * 2)
+        self._enqueue(x)
+
+    def _enqueue(self, x):
+        self._data[self._tail % self._capacity] = x
+        self._tail += 1
 
     def dequeue(self):
-        # TODO - you fill in here.
-        return 0
+        if self.size() > 0:
+            v, self._data[self._head % self._capacity] = self._data[self._head % self._capacity], None
+            self._head += 1
+            return v
 
     def size(self):
-        # TODO - you fill in here.
-        return 0
+        diff = self._tail - self._head
+        return diff if diff >= 0 else self._capacity - diff
+
+    def __iter__(self):
+        p = self._head
+        for _ in range(self.size()):
+            v = self._data[p % self._capacity]
+            p += 1
+            yield v
 
 
 def queue_tester(ops):

768ac10281da6805e11f9cb8780520d77949ad12
selotape <RonVisbord@gmail.com>
2018-07-28 11:43:23 +0300

    number of ways to the top

--- epi_judge_python/number_of_traversals_staircase.py
+++ epi_judge_python/number_of_traversals_staircase.py
@@ -1,10 +1,21 @@
+import functools
+
 from test_framework import generic_test
 
 
 def number_of_ways_to_top(top, maximum_step):
-    # TODO - you fill in here.
-    return 0
+    return _number_of_ways_to_top(top, maximum_step)
 
+
+@functools.lru_cache(maxsize=None)
+def _number_of_ways_to_top(remaining, k):
+    if remaining < 0:
+        return 0
+    if remaining == 0:
+        return 1
+
+    return sum(_number_of_ways_to_top(remaining - step, k)
+               for step in range(1, k + 1))
 
 if __name__ == '__main__':
     exit(
